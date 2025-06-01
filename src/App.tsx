7145c42dd9bf238b263333bc613e56e3d30b2331
tsusu0409 <tsubasa.kawagishi@gamil.com>
2025-06-01 18:10:49 +0900

    create AboutPage

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import BlogPostPage from './pages/BlogPostPage';
 import BlogListPage from './pages/BlogListPage';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/blog/:slug" element={<BlogPostPage />} />
         <Route path="/blogs" element={<BlogListPage />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,19 +1,19 @@
-import './About.css';
+import './ContactPage.css';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const About: React.FC = () => {
+const Contact: React.FC = () => {
     return (
-    <div className="about-page-container">
+    <div className="contact-page-container">
       <Header />
 
       <main className="main-content">
         <div className="container">
             <header className="page-header">
-                <h1>About Us</h1>
+                <h1>Contact</h1>
             </header>
-            <div className="no-abouts-message">
+            <div className="no-contact-message">
               <p>このページは準備中です。</p>
             </div>
         </div>
@@ -24,4 +24,4 @@ const About: React.FC = () => {
   );
 };
 
-export default About;
+export default Contact;
